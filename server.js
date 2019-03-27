45877a90de9b25cafdaf3be83d94dc60358ff5ed
Hoang Nhat Thuan <meosieudang>
2019-03-27 09:39:34 +0700

    update config

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 const app = express();
 
 // bodyparser
@@ -27,6 +28,16 @@ const plates = require("./routes/api/plates");
 app.use("/api/users", users);
 app.use("/api/profiles", profiles);
 app.use("/api/plates", plates);
+
+// Server statis assets if in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // Config
 const port = process.env.PORT || 5000;
