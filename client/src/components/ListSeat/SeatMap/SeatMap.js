0f9520db7299c48d1029b44b77d8c59f7919e5e6
Hoang Nhat Thuan <meosieudang>
2019-04-06 17:37:59 +0700

    'fix'

--- client/src/components/ListSeat/SeatMap/SeatMap.js
+++ client/src/components/ListSeat/SeatMap/SeatMap.js
@@ -11,7 +11,7 @@ import { Typography } from "@material-ui/core";
 class SeatMap extends Component {
   state = {
     value: 0,
-    numberSeat: 0
+    numberSeat: ""
   };
 
   onChange = e => this.setState({ [e.target.name]: e.target.value });
